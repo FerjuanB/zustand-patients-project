0edfa96b21750626a9d137406145d6a563cbb534
Ferjuan <fbatres@gmail.com>
2024-07-19 05:23:08 -0300

    created patient detail and itemDetail

--- src/components/PatientDetail.tsx
+++ src/components/PatientDetail.tsx
@@ -9,14 +9,13 @@ type PatientDetailProps = {
 export const PatientDetail = ({patient} : PatientDetailProps) => {
   return (
     <div className="mx-5 my-10 px-5 py-10 shadow-md md:h-screen rounded-lg bg-white ">
-     <ItemDetail label={"ID"}
-     data={patient.id}/>
-      <p className="font-bold mb-3 text-gray-700 uppercase text-sm" > Nombre:{' '}
-        <span className=" font-normal text-sm normal-case">{patient.name}</span>
-      </p>
-      <p className="font-bold mb-3 text-gray-700 uppercase text-sm" > Nombre:{' '}
-        <span className=" font-normal text-sm normal-case">{patient.name}</span>
-      </p>
+     <ItemDetail label="ID" data={patient.id}/>
+     <ItemDetail label="Nombre" data={patient.name}/>
+     <ItemDetail label="Propietario" data={patient.caretaker}/>
+     <ItemDetail label="Correo" data={patient.email}/>
+     <ItemDetail label="Fecha" data={patient.date.toString()}/>
+     <ItemDetail label="Sintomas" data={patient.symptoms}/>
+     
     </div>
   )
 }
